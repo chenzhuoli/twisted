9826726e452111e139ff7e214c263c6aeae775a9
chenzhuoli <lemonweed@163.com>
2023-10-31 14:12:49 +0800

    Update run_jenkins.sh

--- run_jenkins.sh
+++ run_jenkins.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup ./message_service.py 10.248.192.44 8799 message.log "/home/lixiang/Downloads/Jenkins/workspace/twisted/log" &
+nohup ./message_service.py 10.248.192.44 8799 message.log "/home/lixiang/Downloads/Jenkins/workspace/twisted/log"
